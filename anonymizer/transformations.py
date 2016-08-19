bb052b0f46b87d3bb1220d1e9f1c4fdb71a860a2
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2016-08-19 16:54:59 +0200

    simplest with_closest code

--- anonymizer/transformations.py
+++ anonymizer/transformations.py
@@ -71,9 +71,9 @@ def _name_aggregation(list_of_values):
 
 
 def local_aggregation(serie_init, k, method, unknown=''):
-    ''' 
+    '''
         réalise l'aggregation locale sur une seule variable
-        
+
     '''
     assert serie_init.dtype == 'object'
     assert method in ['into_unknown', 'remove',
@@ -93,11 +93,11 @@ def local_aggregation(serie_init, k, method, unknown=''):
 
     if len(serie) < k:
         return pd.Series(unknown, index=serie_init.index)
-    
+
     if method == 'into_unknown':
-        # si on a que deux valeurs alors le non renseigné devient 
+        # si on a que deux valeurs alors le non renseigné devient
         # facile à retrouver : c'est l'autre valeur
-        #    si remplace k et sur plus d'une modalité on sait que 
+        #    si remplace k et sur plus d'une modalité on sait que
         #    c'est bien anonymisé. sinon, il faut faire autre chose.
         if counts_to_change.sum() >= k  or serie_init.nunique() > 2:
             return serie_init.replace(index_to_change, unknown)
@@ -106,7 +106,7 @@ def local_aggregation(serie_init, k, method, unknown=''):
 
     if method == 'remove':
         return serie_init[~serie_init.isin(index_to_change)]
-    
+
     if 'regroup' in method:
         # on regroupe en priorité les petits groupes entre eux
         # si ça ne suffit pas on va chercher un autre groupe
@@ -114,7 +114,7 @@ def local_aggregation(serie_init, k, method, unknown=''):
         # à k, avec qui regrouper.
         if counts_to_change.sum() >= k:
             pass # rien à faire
-            
+
         if counts_to_change.sum() < k:
             clients_pour_regrouper = counts[counts >= k]
             if len(clients_pour_regrouper) == 0:
@@ -141,7 +141,9 @@ def local_aggregation(serie_init, k, method, unknown=''):
         # on regroupe les nombres qui ne sont pas k-anonymisés avec
         # la valeur la plus proche
         df = pd.DataFrame(counts)
+        df.columns = ['count']
         df['name'] = df.index
+
         def _to_float(str_expression):
             if ' ou ' in str_expression:
                 splittage = str_expression.split(' ou ')
@@ -150,68 +152,29 @@ def local_aggregation(serie_init, k, method, unknown=''):
             else:
                 return float(str_expression)
 
-        df['value'] = df['name'].apply(_to_float)     
-        
-        import pdb; pdb.set_trace()
-        boucle = serie.value_counts()[-1]
-        while boucle < k :
-            serie2 = serie.copy()
-            # Lorsqu'on a une modalité 'x ou y', il faut la transformer en 
-            # valeur numérique pour calculer la distance
-            # => on ne va garder que la première valeur
-            # mais on stocke quand même les "année ou année" pour pouvoir
-            # les modifier à la fin
-            valeurs_splittees = []
-            for x in serie2.unique():
-                if ' ou ' in x:
-                    splittage = x.split(' ou ')
-                    serie2 = serie2.replace(x, splittage[0])
-                    valeurs_splittees.append(splittage)
+        df['value'] = df['name'].apply(_to_float)
 
-            serie2 = serie2.astype(int)
-            counts = serie2.value_counts()
-            counts_to_change = counts[counts < k]
-            index_to_change = counts_to_change.index.tolist()
-            liste_a_comparer = serie2.unique().tolist()
-            modifications = []
-            import pdb;
-            pdb.set_trace()
-            for valeur_a_remplacer in index_to_change: 
-                if valeur_a_remplacer not in modifications: 
-                    liste_a_comparer2 = list(liste_a_comparer) # = copy
-                    liste_a_comparer2.remove(valeur_a_remplacer)
-                    pour_regrouper = [str(valeur_a_remplacer)]
+        modifications = []
+        while df['count'].min() < k:
+            valeur_a_remplacer = df.iloc[-1,:]
+            distance = (df['value'] - valeur_a_remplacer['value'])**2
+            idxmin = distance[distance > 0].idxmin()
 
-                    # on effectue le calcul des distances
-                    # et on les stocke dans un dictionnaire 
-                    d = {}
-                    for i in liste_a_comparer2 :
-                        d[i] = abs(i - valeur_a_remplacer)
+            pour_regrouper = [valeur_a_remplacer['name'], idxmin]
 
-                    # on prend le minimum des distances trouvées
-                    minimum = min(d.items(), key = lambda x: x[1])[0]
-                    # on vérifie pour voir si la modalité de départ
-                    # est présent en l'état dans notre série
-                    # ou sous forme de "année ou année" (cf 1ère étape)
-                    for groupe_splitte in valeurs_splittees:
-                        if pour_regrouper[0] in groupe_splitte:
-                            pour_regrouper = [_name_aggregation(groupe_splitte)]
+            #calcul de la nouvelle modalité
+            new_name = _name_aggregation(pour_regrouper)
+            new_count = df.loc[pour_regrouper]['count'].sum()
+            # introduit un biais quand on agregre deux valeurs
+            # puis une troisième, on tire vers la troisième
+            new_value = df.loc[pour_regrouper]['value'].mean()
+            df.loc[new_name] = [new_count, new_name, new_value]
+            df.drop(pour_regrouper, inplace=True)
+            df.sort_values('count', ascending=False,
+                           inplace=True)
 
-                    # on fait la même opération concernant le minimum trouvé :
-                    # si on a trouvé 2005 mais que l'on a que "2005 ou 2006" 
-                    # comme modalité, il faut le repérer et modifier la valeur
-                    # du string du minimum en conséquence
-                    for modalite in serie.unique().tolist():
-                        if str(minimum) in modalite:
-                            pour_regrouper.append(modalite)
-                    
-                    
-                    #calcul de la nouvelle modalité
-                    new_name = _name_aggregation(pour_regrouper)
-                    serie_init = serie_init.replace(pour_regrouper, new_name)
-                    modifications.append(minimum)
-                    modifications.append(valeur_a_remplacer)
-                    serie = serie_init[serie_init != unknown]
-            
-            boucle = serie.value_counts().min()
+            modifications.append((pour_regrouper, new_name))
+
+        for modification in modifications:
+            serie_init.replace(modification[0], modification[1], inplace=True)
         return serie_init
